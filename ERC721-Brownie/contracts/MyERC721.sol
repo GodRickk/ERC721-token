46473d61dddea6d5ad706b00d4ee55a71f41874d
Ilya Antipin <morlok01@inbox.ru>
2024-09-28 16:33:22 +0300

    refactor: add whitespaces

--- ERC721-Brownie/contracts/MyERC721.sol
+++ ERC721-Brownie/contracts/MyERC721.sol
@@ -29,17 +29,21 @@ contract MyERC721 is ERC721, Ownable{
         MINT_PRICE = mintPrice;
     }
 
+
     function setBaseURI(string memory baseTokenURI) external onlyOwner {
         _baseTokenURI = baseTokenURI;
     }
+
 
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
 
+
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         return string(abi.encodePacked(_baseTokenURI, Strings.toString(tokenId), ".json"));
     }
+
 
     function mint(uint256 amount) public payable {
         require(amount > 0 && amount <= MAX_PER_MINT, "Invalid mint amount");
@@ -54,6 +58,7 @@ contract MyERC721 is ERC721, Ownable{
             _safeMint(msg.sender, tokenId);
         }
     }
+
 
     function withdraw(uint256 amount, address payable to) external onlyOwner {
         require(amount <= address(this).balance, "Insufficient balance");
